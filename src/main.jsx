6046130c1813b37d328782547ed8b1577e3ac65a
M-Ebad003 <ebadmuhammad003@gmail.com>
2025-09-05 18:16:24 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import App from "./App.jsx";
 import { ThemeProvider } from "styled-components";
 import ThemeStyles from "./assets/styles/ThemeStyles.jsx";
 import GlobalStyles from "./assets/styles/GlobalStyles.jsx";
-import Rainbowkit from "./RainbowKit.jsx";
+import RainbowKit from "./RainbowKit.jsx";
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Rainbowkit>
+    <RainbowKit>
       <ThemeProvider theme={ThemeStyles}>
         <GlobalStyles />
         <App />
       </ThemeProvider>
-    </Rainbowkit>
+    </RainbowKit>
   </StrictMode>
 );
